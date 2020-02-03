04b9b1a0b4a6535ba801287ad6aa6344389315ee
sserges <agounkeserge@gmail.com>
2020-02-03 21:04:00 +0000

    Introduction to Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'monAgence';
 
+  constructor() {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyDBuIMQcQE4UYPmm-auWQ7Qw6vheyD8WEk',
+      authDomain: 'post-dfd61.firebaseapp.com',
+      databaseURL: 'https://post-dfd61.firebaseio.com',
+      projectId: 'post-dfd61',
+      storageBucket: 'post-dfd61.appspot.com',
+      messagingSenderId: '456743157182',
+      appId: '1:456743157182:web:d93c17733187b547508def'
+    };
+
+    firebase.initializeApp(firebaseConfig);
+  }
 }
